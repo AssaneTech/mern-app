48231303fe7346688d7a470469b18247859b0185
AssaneTech <assanediouf0063@gmail.com>
2025-12-03 02:43:35 +0000

    settings

--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -1,6 +1,5 @@
 import { create } from "zustand";
-//const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000"; 
-
+const API_URL = import.meta.env.VITE_API_URL
 
 export const useProductStore = create((set) => ({
   products: [],
