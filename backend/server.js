e05de3f9905e6b455744fa69c4faed7f0702afea
AssaneTech <assanediouf0063@gmail.com>
2025-12-03 02:06:10 +0000

    Separate backend and frontend

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from  "./config/db.js";
+import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
-import path from "path";
 
-const PORT = process.env.PORT || 5000
 dotenv.config();
 
-const app = express(); 
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-const __dirname =path.resolve();  
+// Middleware
+app.use(express.json());
 
-app.use(express.json());// allows us to accept JSON data the req.body
+// API routes
+app.use("/api/products", productRoutes);
 
-app.use("/api/products", productRoutes)
+// Test route
+app.get("/", (req, res) => {
+  res.send("Backend API is running...");
+});
 
-
-if (process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get(/.*/, (req,res)=> {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    })
-}
-
- 
-
-app.listen(PORT, "0.0.0.0",() => {
-    connectDB();
-    console.log("Server started at http://localhost:" + PORT);
+// Start server
+app.listen(PORT, "0.0.0.0", () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
 });
